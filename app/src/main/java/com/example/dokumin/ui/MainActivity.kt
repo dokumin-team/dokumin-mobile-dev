fef0f628f3fd0ea7f4b53b2c79311aec5eddfc83
cintarmynti <ramayanticinta@gmail.com>
2024-11-19 15:10:12 +0700

    fix : bottom navigation

--- app/src/main/java/com/example/dokumin/ui/MainActivity.kt
+++ app/src/main/java/com/example/dokumin/ui/MainActivity.kt
@@ -5,7 +5,6 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.dokumin.R
 import com.example.dokumin.databinding.ActivityMainBinding
@@ -20,20 +19,22 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        setSupportActionBar(binding.toolbar)
-
         val navView: BottomNavigationView = binding.navView
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
 
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_document, R.id.navigation_profile, R.id.navigation_folder
+                R.id.navigation_home,
+                R.id.navigation_document,
+                R.id.navigation_profile,
+                R.id.navigation_folder
             )
         )
-        setupActionBarWithNavController(navController, appBarConfiguration)
+        // Hapus setupActionBarWithNavController
         navView.setupWithNavController(navController)
     }
 
+    // Anda bisa menghapus metode ini jika tidak diperlukan
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
         return navController.navigateUp() || super.onSupportNavigateUp()
