a7a915906c814f7e84464fb24c33e1cc9ebdaa4d
cintarmynti <ramayanticinta@gmail.com>
2024-11-24 12:57:21 +0700

    style : profile page

--- app/src/main/java/com/example/dokumin/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/dokumin/ui/profile/ProfileFragment.kt
@@ -28,10 +28,7 @@ class ProfileFragment : Fragment() {
         _binding = FragmentProfileBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textNotifications
-        notificationsViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+
         return root
     }
 
