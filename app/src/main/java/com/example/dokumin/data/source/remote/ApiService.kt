b7d53c95d7bf3c7cc21b6e2042cceabe1248e3b5
cintarmynti <ramayanticinta@gmail.com>
2024-12-06 23:28:46 +0700

    setup service for auth

--- app/src/main/java/com/example/dokumin/data/source/remote/ApiService.kt
+++ app/src/main/java/com/example/dokumin/data/source/remote/ApiService.kt
@@ -1,13 +1,33 @@
 package com.example.dokumin.data.source.remote
 
-import com.example.dokumin.data.source.remote.requests.RegisterRequest
+import com.example.dokumin.data.model.requests.ResendOtpRequest
+import com.example.dokumin.data.model.requests.SignInRequest
+import com.example.dokumin.data.model.responses.SignupModel
+import com.example.dokumin.data.model.requests.SignupRequest
+import com.example.dokumin.data.model.requests.VerifyOtpRequest
 import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface ApiService {
     @POST("users/signup")
-    fun registerUser(
-        @Body registerRequest: RegisterRequest
-    ):Call<String>
+    fun signUpUser(
+        @Body registerRequest: SignupRequest
+    ):Call<SignupModel?>
+
+    fun signInUser(
+        @Body loginRequest: SignInRequest
+    ):Call<SignupModel?>
+
+    @POST("userOTPVerifications/verify")
+    fun verifyOtp(
+        @Body otp: VerifyOtpRequest
+    ) :Call<SignupModel?>
+
+    @POST("userOTPVerifications/resend")
+    fun resendOtp(
+        @Body email: ResendOtpRequest
+    ):Call<SignupModel?>
+
+
 }
