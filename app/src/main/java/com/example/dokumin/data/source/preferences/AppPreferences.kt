2a2ada29c64d926587bde95c58bcfdcb4ae9c997
cintarmynti <ramayanticinta@gmail.com>
2024-12-06 23:04:42 +0700

    setup preference untuk token

---
+++ app/src/main/java/com/example/dokumin/data/source/preferences/AppPreferences.kt
@@ -0,0 +1,49 @@
+package com.example.dokumin.data.source.preferences
+
+import android.content.Context
+
+
+class AppPreferences(val mContext: Context) {
+    companion object {
+        const val prefName = "usr_token"
+        const val tokenKey = "usr_token"
+        const val isFirst = true
+    }
+
+    private val preferences = mContext.getSharedPreferences(prefName, Context.MODE_PRIVATE)
+    private val editor = preferences.edit()
+
+    fun deleteSession() {
+        editor.apply {
+            clear()
+            apply()
+            commit()
+        }
+    }
+
+    fun saveSession(token: String) {
+        editor.apply {
+            putString(tokenKey, token)
+            apply()
+            commit()
+        }
+    }
+
+    fun isFirstTime(): Boolean {
+        return preferences.getBoolean(isFirst.toString(), true)
+    }
+
+    fun updateFirstTime() {
+        editor.apply {
+            putBoolean(isFirst.toString(), false)
+            apply()
+            commit()
+        }
+    }
+
+
+    fun getSession(): String {
+        val token = preferences.getString(tokenKey, "")
+        return token.toString()
+    }
+}
