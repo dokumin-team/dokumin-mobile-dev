433d8ed7f6663b47368e88ba22570f65fe6cd154
cintarmynti <ramayanticinta@gmail.com>
2024-12-06 22:07:14 +0700

    setup gradle dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,4 +63,17 @@ dependencies {
     // Glide dependencies
     implementation("com.github.bumptech.glide:glide:4.15.1")
     annotationProcessor("com.github.bumptech.glide:compiler:4.15.1") // For Glide compiler
+
+    // Retrofit dependencies
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.okhttp3:okhttp:5.0.0-alpha.11")
+    implementation ("com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.11")
+
+    // LiveData Implementation
+    implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
+    implementation ("androidx.lifecycle:lifecycle-livedata-ktx:2.8.7")
+    implementation ("androidx.lifecycle:lifecycle-livedata:2.8.7")
+    implementation ("androidx.lifecycle:lifecycle-extensions:2.2.0")
+
 }
