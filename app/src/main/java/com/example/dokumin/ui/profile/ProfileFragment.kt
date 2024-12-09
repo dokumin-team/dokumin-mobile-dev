6efc85c61f461295ef1c730f43c9439c224aad9c
cintarmynti <ramayanticinta@gmail.com>
2024-12-09 11:25:41 +0700

    logout done

--- app/src/main/java/com/example/dokumin/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/dokumin/ui/profile/ProfileFragment.kt
@@ -1,37 +1,58 @@
 package com.example.dokumin.ui.profile
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.dokumin.R
-import android.content.Intent
+import com.example.dokumin.data.source.preferences.AppPreferences
+import com.example.dokumin.databinding.FragmentProfileBinding
 import com.example.dokumin.ui.about.AboutActivity
 import com.example.dokumin.ui.auth.signin.SignInActivity
 import com.example.dokumin.ui.faq.FaqActivity
-import com.google.android.material.button.MaterialButton
 
 class ProfileFragment : Fragment(R.layout.fragment_profile) {
 
+    private var binding: FragmentProfileBinding? = null
+    private val appPreferences by lazy { AppPreferences(requireContext()) }
+
+    override fun onDestroyView() {
+        binding = null
+        super.onDestroyView()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentProfileBinding.inflate(inflater, container, false)
+        return binding?.root
+    }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         // Navigasi ke FAQ Activity
-        view.findViewById<View>(R.id.FaqMenu).setOnClickListener {
+        binding?.FaqMenu?.setOnClickListener {
             val intent = Intent(requireContext(), FaqActivity::class.java)
             startActivity(intent)
         }
 
         // Navigasi ke About Activity
-        view.findViewById<View>(R.id.AboutMenu).setOnClickListener {
+        binding?.AboutMenu?.setOnClickListener {
             val intent = Intent(requireContext(), AboutActivity::class.java)
             startActivity(intent)
         }
 
         // Logout dan kembali ke Login Activity
-        view.findViewById<MaterialButton>(R.id.logoutButton).setOnClickListener {
+        binding?.logoutButton?.setOnClickListener {
+            appPreferences.deleteSession()
             val intent = Intent(requireContext(), SignInActivity::class.java)
             intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
             startActivity(intent)
+            requireActivity().finish()
         }
     }
 }
