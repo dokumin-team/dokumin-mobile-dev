ef625118bb61ee1197ecc767cec3aadd3bb9a03c
cintarmynti <ramayanticinta@gmail.com>
2024-12-12 05:41:32 +0700

    add dependency ml kit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,7 @@ dependencies {
     implementation(libs.tensorflow.lite.support)
     implementation(libs.tensorflow.lite.metadata)
     implementation(libs.androidx.camera.core)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -87,4 +88,8 @@ dependencies {
     implementation("org.tensorflow:tensorflow-lite-support:0.4.4")
     implementation("org.tensorflow:tensorflow-lite-metadata:0.4.4")
     implementation("org.tensorflow:tensorflow-lite-task-vision:0.4.4")
+
+    //ml kit
+    implementation("com.google.mlkit:barcode-scanning:16.0.0")
+
 }
--- app/src/main/java/com/example/dokumin/ui/home/QrCodeActivity.kt
+++ app/src/main/java/com/example/dokumin/ui/home/QrCodeActivity.kt
@@ -4,53 +4,56 @@ import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Bundle
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.dokumin.R
+import com.example.dokumin.databinding.ActivityQrCodeBinding
 import com.example.dokumin.helper.ImageClassifierHelper
 
 class QrCodeActivity : AppCompatActivity(), ImageClassifierHelper.ClassifierListener {
 
+    private lateinit var binding: ActivityQrCodeBinding
     private lateinit var classifierHelper: ImageClassifierHelper
-    private lateinit var resultTextView: TextView
-    private lateinit var qrImageView: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_qr_code)
-
-        qrImageView = findViewById(R.id.iv_qr)
-        resultTextView = findViewById(R.id.tvResult)
-        val analyzeButton = findViewById<Button>(R.id.btnAnalisisQr)
-
-        val imageUri = intent.extras?.getParcelable<Uri>("imageUri")
-        imageUri?.let {
-            val imageStream = contentResolver.openInputStream(it)
-            val bitmap = BitmapFactory.decodeStream(imageStream)
-            qrImageView.setImageBitmap(bitmap)
-        }
+        binding = ActivityQrCodeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         classifierHelper = ImageClassifierHelper(this, this)
 
-        analyzeButton.setOnClickListener {
+        val imageUri = intent.extras?.getParcelable<Uri>("imageUri")
+        imageUri?.let { uri ->
+            displayImageFromUri(uri)
+        }
+
+        binding.btnAnalisisQr.setOnClickListener {
             imageUri?.let { uri ->
-                val imageStream = contentResolver.openInputStream(uri)
-                val bitmap = BitmapFactory.decodeStream(imageStream)
-                bitmap?.let { bmp ->
-                    classifyImage(bmp)
-                }
+                processAndClassifyImage(uri)
             }
         }
     }
+
+
+    private fun displayImageFromUri(uri: Uri) {
+        val imageStream = contentResolver.openInputStream(uri)
+        val bitmap = BitmapFactory.decodeStream(imageStream)
+        binding.ivQr.setImageBitmap(bitmap)
+    }
+
+    private fun processAndClassifyImage(uri: Uri) {
+        val imageStream = contentResolver.openInputStream(uri)
+        val bitmap = BitmapFactory.decodeStream(imageStream)
+        bitmap?.let { classifyImage(it) }
+    }
+
 
     private fun classifyImage(bitmap: Bitmap) {
         classifierHelper.classifyImage(bitmap)
     }
 
+
     override fun onResults(results: List<Float>, inferenceTime: Long) {
-        val resultText = results.joinToString(prefix = "Hasil: ", separator = ", ")
-        resultTextView.text = "$resultText\nWaktu inferensi: $inferenceTime ms"
+        val resultText = results.joinToString(prefix = "Hasil: ", separator = ", ") { "${it * 100}%" }
+        binding.tvResult.text = "$resultText\nWaktu inferensi: $inferenceTime ms"
     }
 }
